151b178382505fb75638a6c40b5d5f173438ee7d
mdekauwe <mdekauwe@gmail.com>
2018-09-06 11:12:14 +1000

    fixed spelling

--- opt_leaf_longevity.py
+++ opt_leaf_longevity.py
@@ -32,13 +32,13 @@ import constants as c
 def f(ndays, *args, **kws):
     # Optimise leaf lifespan
 
-    (Ci, Tleaf, Par, Rd, Vcmax, Jmax, leaf_construction_cost) = args
+    (Ci, Tleaf, Par, Rd, Vcmax, Jmax, construction_cost) = args
 
     carbon_gain = calc_gain(ndays[0], Ci, Tleaf, Par, Rd, Vcmax, Jmax)
 
     A_day_avg = carbon_gain / ndays[0]
-    maintence_cost = A_day_avg * 0.6 * ndays[0]
-    g = (1.0 / ndays) * (carbon_gain - maintence_cost - leaf_construction_cost)
+    maintenance_cost = A_day_avg * 0.6 * ndays[0]
+    g = (1.0 / ndays) * (carbon_gain - maintenance_cost - construction_cost)
 
     return -1.0 * g
 
@@ -62,35 +62,35 @@ def calc_gain(ndays, Ci, Tleaf, Par, Rd, Vcmax, Jmax):
 
     return carbon_gain
 
-deg2kelvin = 273.15
-Tleaf = 25.0
-Tleaf += deg2kelvin
-Par = 1800.
-Ca = 400.
-Ci = Ca * 0.7
-jv_ratio = 1.67
-Vcmax = 50.0
-Jmax = Vcmax * jv_ratio
-Rd = Vcmax * 0.015
+if __name__ == "__main__":
 
-leaf_construction_cost = 100.0
+    deg2kelvin = 273.15
+    Tleaf = 25.0
+    Tleaf += deg2kelvin
+    Par = 1800.
+    Ca = 400.
+    Ci = Ca * 0.7
+    jv_ratio = 1.67
+    Vcmax = 50.0
+    Jmax = Vcmax * jv_ratio
+    Rd = Vcmax * 0.015
 
-leaf_construct_cost = np.linspace(100.0, 2000.0, 20)
-gain_save = []
-length = []
-for lcost in leaf_construct_cost:
-    # guess at growing season length
-    x0 = [50]
-    result = minimize(f, x0=x0, tol=1E-3,
-                      args=(Ci, Tleaf, Par, Rd, Vcmax, Jmax, lcost),
-                      bounds=[(1, 365*3)])
-    days = result.x[0]
+    leaf_construct_cost = np.linspace(100.0, 8000.0, 20)
+    gain_save = []
+    length = []
+    for lcost in leaf_construct_cost:
+        # guess at growing season length
+        x0 = [50]
+        result = minimize(f, x0=x0, tol=1E-3,
+                          args=(Ci, Tleaf, Par, Rd, Vcmax, Jmax, lcost),
+                          bounds=[(1, 365*3)])
+        days = result.x[0]
 
-    carbon_gain = calc_gain(days, Ci, Tleaf, Par, Rd, Vcmax, Jmax)
-    gain_save.append(carbon_gain / 365.)
-    length.append(days)
+        carbon_gain = calc_gain(days, Ci, Tleaf, Par, Rd, Vcmax, Jmax)
+        gain_save.append(carbon_gain / 365.)
+        length.append(days)
 
-plt.plot(leaf_construct_cost, length, "bo")
-plt.xlabel("Leaf construction cost")
-plt.ylabel("Leaf lifespan (d$^{-1}$)")
-plt.show()
+    plt.plot(leaf_construct_cost, length, "bo")
+    plt.xlabel("Leaf construction cost")
+    plt.ylabel("Leaf lifespan (d$^{-1}$)")
+    plt.show()
